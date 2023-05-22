f4ba138f302fb75b34765b405f761cdf97f10cde
Ripin_Densumite <52118329+RipinDensumite@users.noreply.github.com>
2023-05-23 07:25:36 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Mainpage />
+    </>
+  );
+}
+
+function Mainpage() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +36,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
