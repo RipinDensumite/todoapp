4c8b30c44ea1987cd5304f4d4c5988df85ca333e
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2024-06-10 18:36:47 +0800

    Get api axios learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,80 @@
 import { Button } from "@/components/ui/button"
-import { useState, useEffect } from "react"
-import { toast } from "sonner"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+interface Users {
+  name: string,
+  email: string,
+  age: number | string,
+}
+
+const url = "http://pocketbase-r8gcw4c.172.104.164.95.sslip.io/api/collections/visitors/records"
 
 function App() {
-  const [num, setNum] = useState(0);
+  const [users, setUsers] = useState<Users[]>([])
 
   useEffect(() => {
-    if (num === 10 || num === 20) {
-      toast("Wowowowo please stop", {
-        description: "It harming the environment, so i would like you to stop",
-        action: {
-          label: "Undo",
-          onClick: () => console.log("Undo"),
-        },
-      })
+    const getUser = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log(response);
+        // Map over the items array and return a new object for each user
+        const newUsers = response.data.items.map((item: Users) => ({
+          name: item.name,
+          email: item.email,
+          age: item.age,
+        }));
+        // Update the users state with the new users
+        setUsers(newUsers);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+
+    getUser()
+  }, [])
 
   return (
     <>
-      <section className="flex flex-col items-center justify-center h-screen">
-        <h1>{num}</h1>
-        <Button onClick={() => { setNum(num + 1) }}>Click me</Button>
+      <section className="flex flex-col items-center min-h-screen space-y-5 m-10">
+        <Card className="">
+          <CardHeader>
+            <CardTitle>User input</CardTitle>
+          </CardHeader>
+          <CardContent className="space-y-3">
+            <Input placeholder="name" />
+            <Input placeholder="email" />
+            <Input placeholder="age" />
+          </CardContent>
+          <CardFooter>
+            <Button>Submit</Button>
+          </CardFooter>
+        </Card>
+
+        {users.map((user, index) => {
+          return (
+            <Card key={user.email} className="shadow-lg">
+              <CardHeader>
+                <CardTitle>{user.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Email : {user.email}</p>
+                <p>Age : {user.age}</p>
+              </CardContent>
+              <CardFooter className="flex justify-end">
+                <p>{index + 1}</p>
+              </CardFooter>
+            </Card>
+          );
+        })}
       </section>
     </>
   )
